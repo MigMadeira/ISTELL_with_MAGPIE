672d8603f8770f95953b7dd56a60e1b9335e9e32
MigMadeira <miguel.madeira@tecnico.ulisboa.pt>
2023-08-01 00:26:12 +0100

    adding optimization code

--- ISTELL_different_VV.py
+++ ISTELL_different_VV.py
@@ -12,6 +12,8 @@ from simsopt.util.permanent_magnet_helper_functions import *
 from simsopt.util import FocusData
 from simsopt.util import discretize_polarizations, polarization_axes
 import simsoptpp as sopp
+from simsopt.objectives import SquaredFlux
+from matplotlib import pyplot as plt
 
 t_start = time.time()
 
@@ -175,3 +177,139 @@ for i in range(len(cylinder_list)):
     cylinder_to_vtk(cylinder_list[i], OUT_DIR + f"diagnostics/diagnostic {i}")
 
 pm_opt.write_to_famus(Path(OUT_DIR))
+
+# Set some hyperparameters for the optimization
+algorithm = 'ArbVec'  # Algorithm to use
+nAdjacent = 1  # How many magnets to consider "adjacent" to one another
+nHistory = 300 # How often to save the algorithm progress
+thresh_angle = np.pi # The angle between two "adjacent" dipoles such that they should be removed
+max_nMagnets = 23100
+nBacktracking = 200
+kwargs = initialize_default_kwargs('GPMO')
+kwargs['K'] = max_nMagnets # Maximum number of GPMO iterations to run
+kwargs['nhistory'] = nHistory
+if algorithm == 'backtracking' or algorithm == 'ArbVec_backtracking':
+    kwargs['backtracking'] = nBacktracking  # How often to perform the backtrackinig
+    kwargs['Nadjacent'] = nAdjacent
+    kwargs['dipole_grid_xyz'] = np.ascontiguousarray(pm_opt.dipole_grid_xyz)
+    kwargs['max_nMagnets'] = max_nMagnets
+    if algorithm == 'ArbVec_backtracking':
+        kwargs['thresh_angle'] = thresh_angle
+
+# Optimize the permanent magnets greedily
+t1 = time.time()
+R2_history, Bn_history, m_history = GPMO(pm_opt, algorithm, **kwargs)
+t2 = time.time()
+print('GPMO took t = ', t2 - t1, ' s')
+
+# plot the MSE history
+iterations = np.linspace(0, max_nMagnets, len(R2_history), endpoint=False)
+plt.figure()
+plt.semilogy(iterations, R2_history, label=r'$f_B$')
+plt.semilogy(iterations, Bn_history, label=r'$<|Bn|>$')
+plt.grid(True)
+plt.xlabel('K')
+plt.ylabel('Metric values')
+plt.legend()
+plt.savefig(OUT_DIR + 'GPMO_MSE_history.png')
+
+# Set final m to the minimum achieved during the optimization
+min_ind = np.argmin(R2_history)
+pm_opt.m = np.ravel(m_history[:, :, min_ind])
+print("best result = ", 0.5 * np.sum((pm_opt.A_obj @ pm_opt.m - pm_opt.b_obj) ** 2))
+np.savetxt(OUT_DIR + 'best_result_m=' + str(int(max_nMagnets/ (kwargs['nhistory']) * min_ind )) + '.txt', m_history[:, :, min_ind ].reshape(pm_opt.ndipoles * 3))
+b_dipole = DipoleField(pm_opt.dipole_grid_xyz, m_history[:, :, min_ind ].reshape(pm_opt.ndipoles * 3),
+                       nfp=s.nfp, coordinate_flag=pm_opt.coordinate_flag, m_maxima=pm_opt.m_maxima,)
+b_dipole.set_points(s_plot.gamma().reshape((-1, 3)))
+b_dipole._toVTK(OUT_DIR + "Dipole_Fields_K" + str(int(max_nMagnets / (kwargs['nhistory']) * min_ind)))
+bs.set_points(s_plot.gamma().reshape((-1, 3)))
+Bnormal = np.sum(bs.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
+Bnormal_dipoles = np.sum(b_dipole.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=-1)
+Bnormal_total = Bnormal + Bnormal_dipoles
+# For plotting Bn on the full torus surface at the end with just the dipole fields
+make_Bnormal_plots(b_dipole, s_plot, OUT_DIR, "only_m_optimized_K" + str(int(kwargs['K'] / (kwargs['nhistory']) * min_ind)))
+pointData = {"B_N": Bnormal_total[:, :, None]}
+s_plot.to_vtk(OUT_DIR + "m_optimized_K" + str(int(max_nMagnets / (kwargs['nhistory']) * min_ind)), extra_data=pointData)
+    
+
+# Print effective permanent magnet volume
+B_max = 1.465
+mu0 = 4 * np.pi * 1e-7
+M_max = B_max / mu0 
+dipoles = pm_opt.m.reshape(pm_opt.ndipoles, 3)
+print('Volume of permanent magnets is = ', np.sum(np.sqrt(np.sum(dipoles ** 2, axis=-1))) / M_max)
+print('sum(|m_i|)', np.sum(np.sqrt(np.sum(dipoles ** 2, axis=-1))))
+
+save_plots = True
+if save_plots:
+    # Save the MSE history and history of the m vectors
+    #np.savetxt(
+    #    OUT_DIR / f"mhistory_K{kwargs['K']}_nphi{nphi}_ntheta{ntheta}.txt", 
+    #    m_history.reshape(pm_opt.ndipoles * 3, kwargs['nhistory'] + 1)
+    #)
+    np.savetxt(
+        OUT_DIR + f"R2history_K{max_nMagnets}_nphi{nphi}_ntheta{ntheta}.txt",
+        R2_history
+    )
+    mu0 = 4 * np.pi * 1e-7
+    Bmax = 1.465
+    vol_eff = np.sum(np.sqrt(np.sum(m_history ** 2, axis=1)), axis=0) * mu0 * 2 * s.nfp / Bmax
+    np.savetxt(OUT_DIR + 'eff_vol_history_K' + str(max_nMagnets) + '_nphi' + str(nphi) + '_ntheta' + str(ntheta) + '.txt', vol_eff)
+    
+    # Plot the SIMSOPT GPMO solution
+    bs.set_points(s_plot.gamma().reshape((-1, 3)))
+    Bnormal = np.sum(bs.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
+    make_Bnormal_plots(bs, s_plot, OUT_DIR, "biot_savart_optimized")
+
+    # Look through the solutions as function of K and make plots
+    for k in range(0, kwargs["nhistory"] + 1, 30):
+        mk = m_history[:, :, k].reshape(pm_opt.ndipoles * 3)
+        np.savetxt(OUT_DIR + 'result_m=' + str(int(max_nMagnets / (kwargs['nhistory']) * k)) + '.txt', m_history[:, :, k].reshape(pm_opt.ndipoles * 3))
+        b_dipole = DipoleField(
+            pm_opt.dipole_grid_xyz,
+            mk, 
+            nfp=s.nfp,
+            coordinate_flag=pm_opt.coordinate_flag,
+            m_maxima=pm_opt.m_maxima,
+        )
+        b_dipole.set_points(s_plot.gamma().reshape((-1, 3)))
+        K_save = int(max_nMagnets/ kwargs['nhistory'] * k)
+        b_dipole._toVTK(OUT_DIR + f"Dipole_Fields_K{K_save}_nphi{nphi}_ntheta{ntheta}")
+        print("Total fB = ", 0.5 * np.sum((pm_opt.A_obj @ mk - pm_opt.b_obj) ** 2))
+        Bnormal_dipoles = np.sum(b_dipole.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=-1)
+        Bnormal_total = Bnormal + Bnormal_dipoles
+
+        # For plotting Bn on the full torus surface at the end with just the dipole fields
+        make_Bnormal_plots(b_dipole, s_plot, OUT_DIR, f"only_m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}")
+        pointData = {"B_N": Bnormal_total[:, :, None]}
+        s_plot.to_vtk(OUT_DIR + f"m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}", extra_data=pointData)
+
+    # write solution to FAMUS-type file
+    pm_opt.write_to_famus(Path(OUT_DIR))
+
+# Compute metrics with permanent magnet results
+dipoles_m = pm_opt.m.reshape(pm_opt.ndipoles, 3)
+num_nonzero = np.count_nonzero(np.sum(dipoles_m ** 2, axis=-1)) / pm_opt.ndipoles * 100
+print("Number of possible dipoles = ", pm_opt.ndipoles)
+print("% of dipoles that are nonzero = ", num_nonzero)
+
+# Print optimized f_B and other metrics
+### Note this will only agree with the optimization in the high-resolution
+### limit where nphi ~ ntheta >= 64!
+b_dipole = DipoleField(
+    pm_opt.dipole_grid_xyz,
+    pm_opt.m, 
+    nfp=s.nfp,
+    coordinate_flag=pm_opt.coordinate_flag,
+    m_maxima=pm_opt.m_maxima,
+)
+b_dipole.set_points(s_plot.gamma().reshape((-1, 3)))
+bs.set_points(s_plot.gamma().reshape((-1, 3)))
+Bnormal = np.sum(bs.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
+f_B_sf = SquaredFlux(s_plot, b_dipole, -Bnormal).J()
+print('f_B = ', f_B_sf)
+total_volume = np.sum(np.sqrt(np.sum(pm_opt.m.reshape(pm_opt.ndipoles, 3) ** 2, axis=-1))) * s.nfp * 2 * mu0 / B_max
+print('Total volume = ', total_volume)
+
+t_end = time.time()
+print('Total time = ', t_end - t_start)
